f17c9dff08ea02aeb1559d7667f910bc55cc721c
luteks <sebyackin.daniil@ya.ru>
2025-08-01 22:37:00 +0300

    fix

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/StatService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/StatService.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StatService {
     public static void main(String[] args) {
         SpringApplication.run(StatService.class, args);
-        ;
     }
 }
