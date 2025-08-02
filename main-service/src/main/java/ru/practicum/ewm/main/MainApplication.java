9cc92f5e64e01a7ff30d2840814cd5c2338d8aee
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-08-02 22:17:32 +0300

    Update MainApplication.java

--- main-service/src/main/java/ru/practicum/ewm/main/MainApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/main/MainApplication.java
@@ -2,8 +2,6 @@ package ru.practicum.ewm.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.practicum.ewm.client.StatsClient;
 
 
 @SpringBootApplication
