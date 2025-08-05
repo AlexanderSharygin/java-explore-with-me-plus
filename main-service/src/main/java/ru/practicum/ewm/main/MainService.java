a906364b17ec7ce6bf9d919d7ece44aa24a535e2
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-08-05 23:35:48 +0300

    create config and init db

--- main-service/src/main/java/ru/practicum/ewm/main/MainService.java
+++ main-service/src/main/java/ru/practicum/ewm/main/MainService.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MainApplication {
+public class MainService {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class);
+        SpringApplication.run(MainService.class, args);
     }
 }
