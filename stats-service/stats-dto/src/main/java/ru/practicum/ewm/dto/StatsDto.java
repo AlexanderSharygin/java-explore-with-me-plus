dc20312090fc7148798b0db1c864072824d26e2e
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-08-12 18:31:21 +0300

    fixes

--- stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/StatsDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/StatsDto.java
@@ -6,16 +6,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class StatsDto {
     private String app;
     private String uri;
     private Long hits;
-
-    public StatsDto(String app, String uri, Long hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
 
     public StatsDto(String app, String uri) {
         this.app = app;
