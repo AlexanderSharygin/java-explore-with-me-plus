f9568d78b1998a8f486f35db994d7eb8f28aa29f
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-08-13 08:40:25 +0300

    Update HitDto.java

--- stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/HitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/HitDto.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
