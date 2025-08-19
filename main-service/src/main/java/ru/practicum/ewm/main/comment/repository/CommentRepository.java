0cc294e9f36c529f7e92686b39faba306da068b5
Daniil Sebiakin <sebyackindaniil@ya.ru>
2025-08-20 00:51:06 +0300

    Добавление функционала комментариев

--- main-service/src/main/java/ru/practicum/ewm/main/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/main/comment/repository/CommentRepository.java
@@ -9,9 +9,9 @@ import ru.practicum.ewm.main.comment.model.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<Comment> findAllByAuthor_IdOrderByPublishedOnDesc(Long userId, Pageable pageable);
+    Page<Comment> findAllByAuthor_IdOrderByPublishedOnDesc(Long authorId, Pageable pageable);
 
-    Page<Comment> findAllByAuthor_IdAndEvent_IdOrderByPublishedOnDesc(Long userId, Long eventId, Pageable pageable);
+    Page<Comment> findAllByAuthor_IdAndEvent_IdOrderByPublishedOnDesc(Long authorId, Long eventId, Pageable pageable);
 
     Page<Comment> findAllByEventId(Long eventId, Pageable pageable);
 
