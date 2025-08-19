98981743e8f6665ed45c9aaf838df4f2a8960367
Daniil Sebiakin <sebyackindaniil@ya.ru>
2025-08-20 01:06:36 +0300

    fix

--- main-service/src/main/java/ru/practicum/ewm/main/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/main/comment/repository/CommentRepository.java
@@ -9,9 +9,9 @@ import ru.practicum.ewm.main.comment.model.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<Comment> findAllByAuthorIdOrderByPublishedOnDesc(Long authorId, Pageable pageable);
+    Page<Comment> findAllByAuthorIdOrderByCreatedAtDesc(Long authorId, Pageable pageable);
 
-    Page<Comment> findAllByAuthorIdAndEventIdOrderByPublishedOnDesc(Long authorId, Long eventId, Pageable pageable);
+    Page<Comment> findAllByAuthorIdAndEventIdOrderByCreatedAtDesc(Long authorId, Long eventId, Pageable pageable);
 
     Page<Comment> findAllByEventId(Long eventId, Pageable pageable);
 
